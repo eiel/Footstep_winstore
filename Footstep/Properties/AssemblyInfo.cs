1291bee1192b7b95b65c360a594ace08398ae538
Tomohiko Himura <eiel.hal@gmail.com>
2013-05-03 11:51:07 +0900

    modified mouse and copyright

--- Footstep/Properties/AssemblyInfo.cs
+++ Footstep/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Footstep")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  Tomohiko Himura 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
